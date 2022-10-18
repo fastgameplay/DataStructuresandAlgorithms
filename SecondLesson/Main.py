02b19ed6e6b5eafce54e4b1952b08948d6cef226
fastgameplay <61421145+fastgameplay@users.noreply.github.com>
2022-10-18 15:54:03 +0400

    Final

--- SecondLesson/Main.py
+++ SecondLesson/Main.py
@@ -1,32 +1,40 @@
+
+from audioop import bias
+
+
 def DecToBin(n):
     s = ""
     if(n > 1):
         s += DecToBin(n//2)
-
     return s + str(n % 2)
 
+def BinToDec(n):
+    return int(n,2)
 
-def ToSigned(number): # Signed magnitude
+# Signed magnitude
+def Signed(number): 
     sign = "0"
     if(number < 0): 
         sign = "1"
         number = -number
     return sign + DecToBin(number)
 
-def ToRadix(number): # Radix complement
-    maximum = 10 ** len(str(number))
-    print(maximum - number)
+
+# Radix complement
+def Radix(number, digits = None):
+    if ( digits is None): digits = len(str(number))
+    maximum = 10 ** digits
     return DecToBin(maximum - number)
 
-
-def ToDRadix(number): # Diminished Radix complement
-    maximum = (10 ** len(str(number))) - 1
-    print(maximum - number)
+# Diminished Radix complement
+def DRadix(number, digits = None): 
+    if ( digits is None): digits = len(str(number))
+    maximum = (10 ** digits) - 1
     return DecToBin(maximum - number)
 
-def ToExcess(number, M):
-    pass
+def Excess(number, Bias):
+    return DecToBin(number - Bias)
 
 
-print(ToDRadix(735))
+print((Excess(-2,-8)))
 
